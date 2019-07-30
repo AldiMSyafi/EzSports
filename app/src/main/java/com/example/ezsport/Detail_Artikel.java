5f845efc2c06ea7fc6b0fa335dc6ab2d13da9e32
B-TANK <aldimuhamad_9d@yahoo.com>
2019-07-31 01:48:55 +0700

    baru mulai menampilkan detail post article

---
+++ app/src/main/java/com/example/ezsport/Detail_Artikel.java
@@ -0,0 +1,13 @@
+package com.example.ezsport;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Detail_Artikel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail__artikel);
+    }
+}
