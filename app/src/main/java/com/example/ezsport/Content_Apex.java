c2f5e65457e93b581559ec48305d0d729ba97f96
B-TANK <aldimuhamad_9d@yahoo.com>
2019-07-31 11:40:11 +0700

    membuat page apex dan csgo

---
+++ app/src/main/java/com/example/ezsport/Content_Apex.java
@@ -0,0 +1,13 @@
+package com.example.ezsport;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Content_Apex extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content__apex);
+    }
+}
